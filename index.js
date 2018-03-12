d8206ad826f8d6c0f3553e1c664279607291822b
Michal Parysz <michal.parysz@r.pl>
2018-03-12 17:34:41 +0100

    .

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 class City {
-    constructor(id, cost) {
+    constructor(id, x, y) {
         this.id = id;
-        this.cost = cost;
+        this.x = x;
+        this.y = y;
     }
 }
 
-let cities = new Array();
+let cities = [];
 let countOfCities = 10;
 for(let i = 0; i < countOfCities; i++) {
-    cities.push(new City(i, Math.random() * 10 + 1));
+    cities.push(new City(i, Math.floor(Math.random() * 11), Math.floor(Math.random() * 11)));
 }
+
+_(cities).forEach((city, index, array) => {
+    let solution;
+    _(array).forEach((c) => {
+        // solution.push({})
+    });
+});
+
+
 let theBestSolution = cities.slice();
 console.log(theBestSolution);
