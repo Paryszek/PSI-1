9b41fc9bb93299ab7e2efe510a3d95dd0c3dd827
Michal Parysz <michal.parysz@r.pl>
2018-03-12 18:02:41 +0100

    '.'

--- index.js
+++ index.js
@@ -5,20 +5,34 @@ class City {
         this.y = y;
     }
 }
+class Route {
+    constructor(distance, city) {
+        this.distance = distance;
+        this.visitedCities.push(city);
+    }
+    clone() {
+        return new Route(this.distance).city = this.visitedCities;
+    }
+}
+
+let dist = (cityA, cityB) => Math.sqrt(Math.pow(cityA.x - cityB.x) + Math.pow(cityA.y - cityB.y));
 
 let cities = [];
 let countOfCities = 10;
+
 for(let i = 0; i < countOfCities; i++) {
     cities.push(new City(i, Math.floor(Math.random() * 11), Math.floor(Math.random() * 11)));
 }
-
-_(cities).forEach((city, index, array) => {
-    let solution;
-    _(array).forEach((c) => {
-        // solution.push({})
+let theBestSolution = null;
+_.forEach(cities, (city, index, array) => {
+    let solution = new Route(0, city);
+    _.forEach(array, (c) => {
+        solution.distance += dist(city, c);
+        solution.visitedCities.push(c);
     });
+    theBestSolution = !!theBestSolution && theBestSolution.distance < solution.distance  ? solution.clone() : theBestSolution;
+    console.log(theBestSolution);
 });
 
-
-let theBestSolution = cities.slice();
+// let theBestSolution = cities.slice();
 console.log(theBestSolution);
