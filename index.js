e34509134447fa21eb84738317eaed9275ec6c37
Paryszek <michal.parysz@yahoo.com>
2018-04-07 16:18:47 +0200

    Done, it works

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ class Route {
         return this.distance;
     }
 	traveledDistance () {
-		let distT = 0;
+		let dist = 0;
 		for (let i = 0; i + 1 < this.visitedCities.length; i++) {
-			distT += dist(this.visitedCities[i], this.visitedCities[i + 1]);
+			dist += countDist(this.visitedCities[i], this.visitedCities[i + 1]);
 		}
-		this.setDistance(distT);
+		this.setDistance(dist);
 	}
 	clone () {
 		let cloneRoute = new Route();
@@ -34,54 +34,52 @@ class Route {
 		return cloneRoute;
 	}
 }
+
 let cities = [];
-let countOfCities = 5;
+let countOfCities = 4;
 let theBestSolution = new Route();
 let route = new Route();
 
-let dist = (cityA, cityB) => { 
+
+let countDist = (cityA, cityB) => { 
 	return Math.floor(Math.sqrt(Math.pow(cityA.x - cityB.x, 2) + Math.pow(cityA.y - cityB.y, 2)));
 };
+
+let swap = (arr, i, j) => {
+	let temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+	return arr;
+}
 
 let isBestRoute = (routeOne, routeTwo) => {
 	if (routeTwo.visitedCities.length !== 0) {
 		routeOne.traveledDistance();
 		routeTwo.traveledDistance();
-		if (routeOne.distance < routeTwo.distance) {
-			console.log(routeOne.distance < routeTwo.distance);
-		}
 		return routeOne.distance < routeTwo.distance;
 	}
 }
-let func = (route, arrayOfCities) => {
-	if(arrayOfCities.length !== 0) {
+let TSP = (index, route, arrayOfCities) => {
+	if(index !== countOfCities - 1) {
 		for (let i = 0; i < arrayOfCities.length; i++) {
-			let justRemoved = arrayOfCities[0].clone();
-			arrayOfCities = arrayOfCities.slice(1, arrayOfCities.length);
+			arrayOfCities = swap(arrayOfCities, index, i);
+			route.visitedCities = arrayOfCities.slice();
 			let newRoute = route.clone();
-			newRoute.visitedCities.unshift(justRemoved);
-			func(newRoute, arrayOfCities);
-			arrayOfCities.unshift(justRemoved);
+			TSP(index + 1, newRoute, arrayOfCities);
+			arrayOfCities = swap(arrayOfCities, index, i);
 		}
 	} else {
 		if (isBestRoute(route, theBestSolution.clone())) {
-			console.log(route.distance < theBestSolution.distance);
-			if (route.distance < theBestSolution.distance) {
-				console.log(route.distance < theBestSolution.distance);
-			}
 			theBestSolution = route.clone();
 		}
 	}
 }
 
+// Init the cities
 for (let i = 0; i < countOfCities; i++) {
 	cities.push(new City(i, Math.floor(Math.random() * 101), Math.floor(Math.random() * 101)));
 	theBestSolution.visitedCities.push(cities[i]);
 }
 
-
-func(route, cities.slice());
-
-
-
+TSP(0, route, cities.slice());
 console.log('Najkrotszy dystans: ' + theBestSolution.getDistance());
